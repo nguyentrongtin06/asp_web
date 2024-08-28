325abe86f249c105c5f9943d65df0b1aea01550e
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-08-28 14:29:44 +0700

    bo sung baitap2

--- baitap1/baitap1/obj/Debug/net8.0/baitap1.AssemblyInfo.cs
+++ baitap1/baitap1/obj/Debug/net8.0/baitap1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("baitap1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb1a85725bd51bf6cb09ff3be680d8afe0fcc0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("baitap1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("baitap1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
