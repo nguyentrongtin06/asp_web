80f8bdc51d4348a75cfed308c69ae32b5567f54c
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-08-28 14:59:14 +0700

    hoan thanh bai tap 02b

---
+++ baitap2/baitap2/Controllers/TheLoaiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace baitap2.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- baitap2/baitap2/obj/Debug/net8.0/baitap2.AssemblyInfo.cs
+++ baitap2/baitap2/obj/Debug/net8.0/baitap2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("baitap2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bb1a85725bd51bf6cb09ff3be680d8afe0fcc0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fa2527af47b136845cd8b9792a3995aa8c6e5de")]
 [assembly: System.Reflection.AssemblyProductAttribute("baitap2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("baitap2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
