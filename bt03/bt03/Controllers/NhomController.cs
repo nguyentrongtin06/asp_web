d3bbb9f535f451a8c2c91bb04f7ef605b3f25343
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-08-31 14:29:32 +0700

    bt03

---
+++ bt03/bt03/Controllers/NhomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bt03.Controllers
+{
+    public class NhomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ bt03/bt03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bt03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
