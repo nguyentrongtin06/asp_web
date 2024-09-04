b745b29ed39eeeecf19ebff25872267ffc2cdb60
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-09-04 15:03:23 +0700

    bai tap 4

---
+++ bt04/Controllers/TheLoaiController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bt04.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Edit(int id)
+        {
+            return Ok("Id: "+ id);
+        }
+
+        public IActionResult Detail(int id,string ten)
+        {
+            //return Content("Id: " + id +" ;ten: "+ten);
+            return Content(string.Format("Id:{0}; ten: {1} ", id,ten));
+        }
+
+        public IActionResult Show(List<string> categories)
+        {
+            string content = "danh sach the loai: ";
+            foreach (var category in categories)
+            {
+                content = content + " " + category + ", ";
+            }
+            return Content(content);
+        }
+    }
+}
