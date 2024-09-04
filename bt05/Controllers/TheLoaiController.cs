1c5e3c0c984b47b9504e3b8ca302ce130109ce7c
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-09-04 15:04:04 +0700

    bai tap 5

---
+++ bt05/Controllers/TheLoaiController.cs
@@ -0,0 +1,25 @@
+﻿using bt05.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bt05.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Ngay = "Ngày 28";
+            ViewBag.Thang = "Tháng 30";
+            ViewData["Nam"] = "năm 100 nhớ";
+            return View();
+        }
+        public IActionResult Index2()
+        {
+            var theloai = new TheLoaiViewModel
+            {
+                Id = 1,
+                Name = "trinh tham"
+            };
+            return View(theloai);
+        }
+    }
+}
