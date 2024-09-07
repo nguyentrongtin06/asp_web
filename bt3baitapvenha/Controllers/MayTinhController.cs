8c883fe7c9c8e15dc254979a281a734c89a1ef57
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-09-07 11:03:15 +0700

    nop bai tap ve nha

---
+++ bt3baitapvenha/Controllers/MayTinhController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace bt3baitapvenha.Controllers
+{
+    public class MayTinhController : Controller
+    {
+        public IActionResult MayTinh(double a, double b, string pheptinh)
+        {
+            {
+                double ketqua = 0;
+                switch (pheptinh)
+                {
+                    case "cong":
+                        ketqua = a + b;
+                        break;
+                    case "tru":
+                        ketqua = a - b;
+                        break;
+                    case "nhan":
+                        ketqua = a * b;
+                        break;
+                    case "chia":
+                        if (b != 0)
+                            ketqua = (double)a / b;
+                        else
+                            ViewBag.Error = "Không thể chia cho 0";
+                        break;
+                    default:
+                        ViewBag.Error = "Phép tính không hợp lệ";
+                        break;
+                }
+                ViewBag.KetQua = ketqua;
+                return View();
+            }
+        }
+    }
+}
---
+++ bt3baitapvenha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bt3baitapvenha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
