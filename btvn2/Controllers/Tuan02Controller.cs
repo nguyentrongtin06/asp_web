adaa91b045317c3c90bf62aac8ad5929f63198fc
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-09-07 11:42:25 +0700

    chua hoan thanh xog con dang sai

---
+++ btvn2/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace btvn2.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Nguyen Trong Tin";
+            ViewBag.MSSV = "1822030064";
+            ViewBag.Nam = 2003;
+            return View();
+        }
+    }
+}
