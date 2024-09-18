b8cf336fc5441a59a2a27f29e96ef8ae740c06d0
Nguyen Trong Tin <nguyentrongtin810@gmail.com>
2024-09-18 15:21:50 +0700

    hoan thanh bai tap 7

---
+++ bai07/Controllers/TheLoaiController.cs
@@ -0,0 +1,37 @@
+﻿using bai07.Data;
+using bai07.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bai07.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public TheLoaiController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var theloai = _db.TheLoai.ToList();
+            ViewBag.theloai=theloai;
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(TheLoai theLoai)
+        {
+
+            _db.TheLoai.Add(theLoai);
+
+            _db.SaveChanges();
+
+            return View();
+        }
+    }
+}
---
+++ bai07/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using bai07.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace bai07.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<TheLoai> TheLoai { get; set; }
+    }
+}
